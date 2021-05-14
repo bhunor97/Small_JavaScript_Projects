65fb5c82be361aafb6872d1b1026872d0c3f0ef5
Bartha Hunor <bhun0r97@gmail.com>
2021-05-14 13:19:12 +0200

    Calculator  - display test
    .gitignore file + pushButton function added

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,2 +1,18 @@
-// data attributes must be inside of [] - when selected
-const symbolButtons = document.querySelectorAll("[data-symbol]");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".calcButton");
+
+let keys = Object.keys(buttons);
+let values = Object.values(buttons);
+let entries = Object.entries(buttons);
+
+function pushButton() {
+  for (let i = 0; i < values.length; i++) {
+    const newValues = values[i];
+    const valuesText = values[i].innerHTML;
+    newValues.onclick = () => {
+      display.innerHTML = valuesText;
+    };
+  }
+}
+
+console.log(pushButton());
