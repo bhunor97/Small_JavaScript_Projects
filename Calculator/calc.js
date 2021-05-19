f48d33c75d9d48d620f42e072f380a3ace033655
Bartha Hunor <bhun0r97@gmail.com>
2021-05-19 10:53:06 +0200

    multiply / divide / equal - buttons added

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,22 +1,62 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".calcButton");
 
-let keys = Object.keys(buttons);
-let values = Object.values(buttons);
-let entries = Object.entries(buttons);
+let buttonValues = Object.values(buttons);
+
+let number = 0;
+display.innerHTML = "";
+let operator;
 
 function pushButton() {
-  for (let i = 0; i < values.length; i++) {
-    const newValues = values[i];
-    const valuesText = values[i].innerHTML;
+  // DISPLAY
+  for (let i = 5; i < 15; i++) {
+    const newValues = buttonValues[i];
+    const valuesText = buttonValues[i].innerHTML;
     newValues.onclick = () => {
-      display.innerHTML = valuesText;
+      if (display.innerHTML == "") {
+        display.innerHTML = valuesText;
+      } else {
+        display.innerHTML = parseInt(display.innerHTML) + valuesText;
+      }
+      // console.log(display.innerHTML);
     };
   }
-  const clearButton = values[16];
+
+  // CLEAR BUTTON
+  const clearButton = buttonValues[16];
   clearButton.onclick = () => {
     display.innerHTML = "";
   };
+
+  // MULTIPLY
+  const asterisk = buttonValues[0];
+  asterisk.onclick = () => {
+    number = parseInt(display.innerHTML);
+    display.innerHTML = "";
+    operator = 1;
+  };
+
+  // DIVIDE
+  const divide = buttonValues[1];
+  divide.onclick = () => {
+    number = parseInt(display.innerHTML);
+    display.innerHTML = "";
+    operator = 2;
+  };
+
+  // EQUAL
+  const equal = buttonValues[15];
+  equal.onclick = () => {
+    // multiplication
+    if (operator == 1) {
+      display.innerHTML = display.innerHTML * number;
+    }
+    // division
+    else if (operator == 2) {
+      display.innerHTML = number / display.innerHTML;
+      console.log(display.innerHTML);
+    }
+  };
 }
 
-console.log(pushButton());
+pushButton();
