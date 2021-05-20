fe6032669e8f27666dff02e07fc8a01915ada1bb
Bartha Hunor <bhun0r97@gmail.com>
2021-05-20 20:22:44 +0200

    onclick - error display

--- Tip_Form/tip.js
+++ Tip_Form/tip.js
@@ -2,6 +2,7 @@
 const price = document.getElementById("howMuch");
 const people = document.getElementById("howMany");
 // dropdown
+const selectList = document.getElementById("select");
 const choose = document.getElementById("choose");
 const option20perc = document.getElementById("great_20");
 const option10perc = document.getElementById("good_10");
@@ -17,3 +18,13 @@ const tipDisp = document.getElementById("tipAmountDiv");
 const totalDisp = document.getElementById("totalAmountDiv");
 const eachOwesDisp = document.getElementById("eachOwesDiv");
 resultsDiv.style.display = "none";
+
+calcButton.onclick = () => {
+  // ERROR DISPLAY
+  if (price.value == "" || people.value == "" || choose) {
+    errorDisp.style.display = "block";
+    setTimeout(() => {
+      errorDisp.style.display = "none";
+    }, 3000);
+  }
+};
