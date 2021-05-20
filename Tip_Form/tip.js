b72109a258e2e9f8b37b09d565fac20a92528042
Bartha Hunor <bhun0r97@gmail.com>
2021-05-20 19:10:57 +0200

    js variables declared

--- Tip_Form/tip.js
+++ Tip_Form/tip.js
@@ -1,1 +1,19 @@
-
+// input
+const price = document.getElementById("howMuch");
+const people = document.getElementById("howMany");
+// dropdown
+const choose = document.getElementById("choose");
+const option20perc = document.getElementById("great_20");
+const option10perc = document.getElementById("good_10");
+const option2perc = document.getElementById("bad_2");
+// button
+const calcButton = document.getElementById("calcButton");
+// error display
+const errorDisp = document.getElementById("errorDisplay");
+errorDisp.style.display = "none";
+// result display
+const resultsDiv = document.getElementById("resultsDisplayDiv");
+const tipDisp = document.getElementById("tipAmountDiv");
+const totalDisp = document.getElementById("totalAmountDiv");
+const eachOwesDisp = document.getElementById("eachOwesDiv");
+resultsDiv.style.display = "none";
