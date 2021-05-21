6994821a3ca20a349c195a1d2c938432086d4661
Bartha Hunor <bhun0r97@gmail.com>
2021-05-21 11:22:58 +0200

    error display - fix_1

--- Tip_Form/tip.js
+++ Tip_Form/tip.js
@@ -1,12 +1,9 @@
 // input
-const price = document.getElementById("howMuch");
-const people = document.getElementById("howMany");
+const howMuch = document.getElementById("howMuch");
+const howMany = document.getElementById("howMany");
 // dropdown
-const selectList = document.getElementById("select");
-const choose = document.getElementById("choose");
-const option20perc = document.getElementById("great_20");
-const option10perc = document.getElementById("good_10");
-const option2perc = document.getElementById("bad_2");
+const selectList = document.getElementById("serviceList");
+
 // button
 const calcButton = document.getElementById("calcButton");
 // error display
@@ -14,17 +11,42 @@ const errorDisp = document.getElementById("errorDisplay");
 errorDisp.style.display = "none";
 // result display
 const resultsDiv = document.getElementById("resultsDisplayDiv");
-const tipDisp = document.getElementById("tipAmountDiv");
+const amountDisp = document.getElementById("tipAmountDiv");
 const totalDisp = document.getElementById("totalAmountDiv");
 const eachOwesDisp = document.getElementById("eachOwesDiv");
-resultsDiv.style.display = "none";
+resultsDiv.style.display = "block";
 
 calcButton.onclick = () => {
   // ERROR DISPLAY
-  if (price.value == "" || people.value == "" || choose) {
+  if (
+    howMuch.value == "" ||
+    howMany.value == "" ||
+    selectList.options[selectList.selectedIndex].text == "Choose..."
+  ) {
     errorDisp.style.display = "block";
     setTimeout(() => {
       errorDisp.style.display = "none";
     }, 3000);
   }
+  // if (selectList.options[selectList.selectedIndex].text == "Choose...") {
+  //   errorDisp.style.display = "block";
+  // } else {
+  //   errorDisp.style.display = "none";
+  // }
 };
+
+// selectList.addEventListener("change", () => {
+//   if (selectList.options[selectList.selectedIndex].text == "Choose...") {
+//     console.log("yey");
+//   } else {
+//     console.log(selectList.options[selectList.selectedIndex].text);
+//   }
+// });
+
+// $10 --> price
+// 2ppl --> people
+// good-10perc --> option
+
+// Tip Amount $ 1.00 -->  Math.round((price/ option.value) * 100)
+// Total Amount $ 11.00 --> price + tip
+// Each Person Owes $ 5.50 --> (price + tip) / people
