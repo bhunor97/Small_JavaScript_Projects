a62dfe5180fef4a3d73f4c710160f59843636608
Bartha Hunor <bhun0r97@gmail.com>
2021-05-24 14:39:03 +0200

    output container / buttons / css flexbox style added

--- ToDo_List/toDo.js
+++ ToDo_List/toDo.js
@@ -6,16 +6,49 @@ const clearButton = document.getElementById("clearButton");
 const errorDiv = document.getElementById("errorDiv");
 errorDiv.style.display = "none";
 
+// addButton.onclick = () => {
+//   if (!inputText.value == "") {
+//     let newDiv = document.createElement("div");
+//     let textNode = document.createTextNode(inputText.value);
+//     newDiv.appendChild(textNode);
+//     outputDiv.appendChild(newDiv);
+
+//     newDiv.insertAdjacentHTML("afterend", "EDIT");
+//     newDiv.insertAdjacentHTML("afterend", "DELETE ");
+//     inputText.value = "";
+//     console.log(outputDiv);
+//   } else {
+//     errorDiv.style.display = "flex";
+//     setTimeout(() => {
+//       errorDiv.style.display = "none";
+//     }, 2000);
+//   }
+// };
+
 addButton.onclick = () => {
   if (!inputText.value == "") {
+    // SECTION CONTAINER
+    let outputContainer = document.createElement("section");
+    outputContainer.className = "outputContainer";
+    outputDiv.appendChild(outputContainer);
+    // ADD TEXT
     let newDiv = document.createElement("div");
-    let textNode = document.createTextNode(inputText.value);
-    newDiv.appendChild(textNode);
-    outputDiv.appendChild(newDiv);
+    newDiv.innerHTML = inputText.value;
+    newDiv.className = "newOutputDiv";
+    outputContainer.appendChild(newDiv);
+    // DELETE BUTTON
+    let newDelButton = document.createElement("button");
+    newDelButton.className = "newDelButt";
+    newDelButton.innerHTML = "Delete";
+    outputContainer.appendChild(newDelButton);
+    // EDIT BUTTON
+    let newEditButton = document.createElement("button");
+    newEditButton.innerHTML = "Edit";
+    newEditButton.className = "newEditButt";
+    outputContainer.appendChild(newEditButton);
 
-    newDiv.insertAdjacentHTML("afterend", "EDIT");
-    newDiv.insertAdjacentHTML("afterend", "DELETE ");
     inputText.value = "";
+    console.log(outputDiv);
   } else {
     errorDiv.style.display = "flex";
     setTimeout(() => {
