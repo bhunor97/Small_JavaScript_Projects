89d7f9712e2c9c443d62522556ae71307b9255bd
Bartha Hunor <bhun0r97@gmail.com>
2021-06-02 17:01:13 +0200

    JS variables + add question / close buttons

---
+++ Flashcard/flashcard.js
@@ -0,0 +1,22 @@
+const addQuestionButt = document.getElementById("addQuestButt");
+const inputSection = document.getElementById("inputDiv");
+const xButt = document.getElementById("xButt");
+const alertDiv = document.getElementById("alertDiv");
+const form = document.getElementById("formSection");
+const questInput = document.getElementById("questTxtArea");
+const answInput = document.getElementById("answTxtArea");
+const saveButt = document.getElementById("saveButt");
+const outputDiv = document.getElementById("outputDiv");
+
+inputSection.style.display = "none";
+alertDiv.style.display = "none";
+outputDiv.style.display = "none";
+
+// ADD QUESTION BUTTON
+addQuestionButt.onclick = () => {
+  inputSection.style.display = "block";
+};
+// 'X' - BUTTON
+xButt.onclick = () => {
+  inputSection.style.display = "none";
+};
