035f3815ce199c2b51a62949cea8ae99ffd679f7
Bartha Hunor <bhun0r97@gmail.com>
2021-06-02 19:24:52 +0200

    ui / createCard proto / newElements / alerts / clear btn

--- Flashcard/flashcard.js
+++ Flashcard/flashcard.js
@@ -1,15 +1,18 @@
 const addQuestionButt = document.getElementById("addQuestButt");
 const inputSection = document.getElementById("inputDiv");
 const xButt = document.getElementById("xButt");
-const alertDiv = document.getElementById("alertDiv");
+const emptyAlertDiv = document.getElementById("emptyAlertDiv");
+const clearedAlertDiv = document.getElementById("listClearedDiv");
 const form = document.getElementById("formSection");
 const questInput = document.getElementById("questTxtArea");
 const answInput = document.getElementById("answTxtArea");
 const saveButt = document.getElementById("saveButt");
+const clearAllButt = document.getElementById("clearAllButt");
 const outputDiv = document.getElementById("outputDiv");
 
 inputSection.style.display = "none";
-alertDiv.style.display = "none";
+emptyAlertDiv.style.display = "none";
+clearedAlertDiv.style.display = "none";
 outputDiv.style.display = "none";
 
 // FLASHCARD CLASS
@@ -20,7 +23,55 @@ class Flashcard {
   }
 }
 
+// UI CONSTRUCTOR
+function UI() {}
+
+// CREATE CARD
+UI.prototype.createCard = (flashcard) => {
+  if (!questInput.value == "" || !answInput.value == "") {
+    // new section
+    const newSection = document.createElement("section");
+    newSection.className = "newSection";
+    outputDiv.appendChild(newSection);
+
+    // new inner div
+    let newInnerDiv = document.createElement("div");
+    newInnerDiv.className = "newInnerDiv";
+    newInnerDiv.innerHTML = `
+    <li class="questionLi">${flashcard.question}</li>
+    <a class="showHideLink" href="#">Show/Hide Answer</a>
+    <li class="answerLi">${flashcard.answer}</li>
+    `;
+    newSection.appendChild(newInnerDiv);
+
+    // delete button div
+    let newButtDiv = document.createElement("div");
+    newButtDiv.className = "newButtDiv";
+    newButtDiv.innerHTML = `
+  <button class="newEditButt">Edit</button>
+  <button class="newDelButt">Delete</button>
+  `;
+    newInnerDiv.appendChild(newButtDiv);
+    // clear fields
+    questInput.value = "";
+    answInput.value = "";
+  } else {
+    alertEmptyFunction();
+  }
+};
+
+// FORM SUBMIT
 form.addEventListener("submit", function (e) {
+  outputDiv.style.display = "block";
+
+  const question = questInput.value;
+  const answer = answInput.value;
+
+  const ui = new UI();
+  const flashcard = new Flashcard(question, answer);
+  ui.createCard(flashcard);
+
+  console.log(outputDiv);
   e.preventDefault();
 });
 
@@ -31,4 +82,24 @@ addQuestionButt.onclick = () => {
 // 'X' - BUTTON
 xButt.onclick = () => {
   inputSection.style.display = "none";
+  outputDiv.style.display = "none";
+};
+// CLEAR ALL BUTTON
+clearAllButt.onclick = () => {
+  outputDiv.innerHTML = "";
+  alertClearedFunction();
+};
+// ALERT FUNCTION
+alertEmptyFunction = () => {
+  emptyAlertDiv.style.display = "flex";
+  setTimeout(function () {
+    emptyAlertDiv.style.display = "none";
+  }, 2000);
+};
+// CLEAR ALERT FUNCTION
+alertClearedFunction = () => {
+  clearedAlertDiv.style.display = "flex";
+  setTimeout(function () {
+    clearedAlertDiv.style.display = "none";
+  }, 2000);
 };
