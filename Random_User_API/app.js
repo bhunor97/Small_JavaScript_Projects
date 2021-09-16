ef710f3aa69ad4e91777384fd78cc622cf3185bc
Bartha Hunor <bhun0r97@gmail.com>
2021-09-16 18:08:33 +0200

    user info / list items fixed

--- Random_User_API/app.js
+++ Random_User_API/app.js
@@ -1,21 +1,28 @@
 const image = document.getElementById("image_div");
 const listUl = document.getElementById("name_list");
 const generateButton = document.getElementById("generate_person_button");
+const title = document.getElementById("title");
 
 generateButton.addEventListener("click", () => {
-  fetch("https://randomuser.me/api/")
+  title.style.display = "none";
+  fetch("https://randomuser.me/api/?results=100")
     .then((res) => res.json())
     .then((data) => {
       listUl.innerHTML = `
-        <li>Name: ${data.name}</li>
-        <li>Last Name: ${data.name}</li>
-        <li>Location: ${data.name}</li>
-        <li>Phone: ${data.name}</li>
-        <li>Email: ${data.name}</li>
-        `;
+      <li><b>Name:</b> ${
+        data.results[1].name.first + " " + data.results[1].name.last
+      }</li>
+      <li><b>Location:</b> ${data.results[1].location.city}
+      </li>
+      <li>
+      <b>Email:</b> ${data.results[1].email}
+      </li>
+      <li>
+      <b>Phone:</b> ${data.results[1].phone}
+      </li>
+      `;
+      image.innerHTML = `
+      <img class="img-generated" src="${data.results[1].picture.large}">
+      `;
     });
 });
-
-fetch("https://randomuser.me/api/")
-  .then((res) => res.json())
-  .then((data) => console.log(data.results));
